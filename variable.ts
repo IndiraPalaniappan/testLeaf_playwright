d0b21c66088e874ed9544609892368b7dc9e0615
IndiraPalaniappan <136819594+IndiraPalaniappan@users.noreply.github.com>
2025-08-16 23:31:27 +0530

    Add files via upload

---
+++ variable.ts
@@ -0,0 +1,13 @@
+
+var firstName="abi"
+console.log(typeof(firstName))
+
+var companyName="abc"
+console.log(typeof(companyName))
+
+var mobileNumber=9876543210
+console.log(typeOf(mobileNumber))
+
+
+var isAutomation=true
+console.log(typeOf(isAutomation))
